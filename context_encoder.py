5dc6d8fefe5ddde34752f3f23393e2b22e2866fc
thesigmaguy <sdhawal2k8@gmail.com>
2020-04-11 22:31:25 +0530

    code uploaded

---
+++ context_encoder.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import glob
+
+from PIL import Image
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+from torchvision import transforms
+
+import torch
+import torch.nn.functional as F
+from torch import optim
+from torch.autograd import Variable
+
+from generator import generator as Generator
+from discriminator import discriminator as Discriminator
+from data_loader import image_loader
+from context_encoder_test import run_test
+
+data_path = sys.argv[1] +'/'
+
+results_path = sys.argv[2] + '/'
+
+log_path = sys.argv[3] + '/'
+
+check_points_path = sys.argv[4] + '/'
+
+model_path = sys.argv[5] + '/'
+
+test_path = sys.argv[6] + '/'
+
+test_output_path = sys.argv[7] +'/'
+
+img_x, img_y=128, 128
+channels=3
+mask_x, mask_y=64, 64
+epochs=3000
+
+#required --> transforms  Resize, to-tensor-for-pytorch, normalize
+
+transforms_ =   [
+                    transforms.Resize((img_x, img_y)),
+                    transforms.ToTensor(),
+                    transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+                ]
+
+
+adversarial_loss = torch.nn.MSELoss()
+pixelwise_loss = torch.nn.L1Loss()
+
+generator = Generator()
+discriminator = Discriminator()
+
+generator.cuda()
+discriminator.cuda()
+pixelwise_loss.cuda()
+adversarial_loss.cuda()
+
+train_data_loader = DataLoader(
+                                image_loader(path= data_path + '*.JPG', transforms_=transforms_),
+                                batch_size=256,
+                                shuffle=True,
+                                num_workers=10
+                              )
+
+val_data_loader = DataLoader(
+                                image_loader(path=data_path + '*.JPG', transforms_=transforms_, mode='val'),
+                                batch_size=256,
+                                shuffle=False,
+                                num_workers=10
+                             )
+
+optimizer_g = torch.optim.Adam(generator.parameters(), lr=.0001, betas=(.5, .999))
+optimizer_d = torch.optim.Adam(discriminator.parameters(), lr=.0001, betas=(.5, .999))
+
+Tensor = torch.cuda.FloatTensor
+
+def save_sample(epoch):
+    global val_data_loader
+    samples, masked_samples, i = next(iter(val_data_loader))
+    samples = Variable(samples.type(Tensor))
+    masked_samples = Variable(masked_samples.type(Tensor))
+    i = i[0].item()
+    gen_mask = generator(masked_samples)
+    filled_samples = masked_samples.clone()
+    filled_samples[:, :, i:i+mask_x, i:i+mask_y] = gen_mask
+    sample = torch.cat((masked_samples.data, filled_samples.data, samples.data), -2)
+    save_image(sample, results_path + 'epoch-%d.jpg'%(epoch), nrow=6, normalize=True)
+
+log_loss_file = open(log_path + 'log_loss_file.csv', 'w')
+log_loss_file.write('Epoch,Batch,real_loss,fake_loss,d_loss,g_adv,g_pixel,g_loss\n')
+
+
+import time 
+
+for epoch in range(1, 1 + epochs):
+    epcoch_start = time.time()
+    for i , (imgs, masked_imgs, masked_parts) in enumerate(train_data_loader):
+
+        valid = Variable(Tensor(imgs.shape[0], *(1, 4, 4)).fill_(1.0), requires_grad=False)
+        fake = Variable(Tensor(imgs.shape[0], *(1, 4, 4)).fill_(.0), requires_grad=False)
+
+        imgs = Variable(imgs.type(Tensor))
+        masked_imgs = Variable(masked_imgs.type(Tensor))
+        masked_parts = Variable(masked_parts.type(Tensor))
+
+        #train generator
+        optimizer_g.zero_grad()
+
+        #generated parts
+        generated_parts = generator(masked_imgs)
+
+        discriminator_result = discriminator(generated_parts)
+
+        g_adv = adversarial_loss(discriminator_result, valid)
+        g_pixel = pixelwise_loss(generated_parts, masked_parts)
+
+        #combined loss
+        g_loss = .001*g_adv + .999*g_pixel
+
+        g_loss.backward()
+
+        optimizer_g.step()
+
+        #train discriminator
+        optimizer_d.zero_grad()
+
+        real_loss = adversarial_loss(discriminator(masked_parts), valid)
+        fake_loss = adversarial_loss(discriminator(generated_parts.detach()), fake)
+
+        d_loss = .5 * (real_loss + fake_loss)
+
+        d_loss.backward()
+
+        optimizer_d.step()
+
+        print(
+                'Epoch %d/%d Batch %d/%d [D loss: %f] [G adv: %f, pixel: %f]'
+                %(epoch, epochs, i+1, len(train_data_loader), d_loss.item(), g_adv.item(), g_pixel.item())
+             )
+        
+        if i%len(train_data_loader) == 0:
+            metrics = (epoch, i+1, real_loss.item(), fake_loss.item(), d_loss.item(), g_adv.item(), g_pixel.item(), g_loss.item())
+            log_metrics = '%f,'*len(metrics)
+            log_loss_file.write(log_metrics[:-1]%metrics + '\n')
+            log_loss_file.flush()
+
+            batches_done = epoch * len(train_data_loader) + i
+
+    if epoch % 50 == 1:
+        save_sample(epoch)
+        check_point = {
+                        'epoch':                        epoch,
+                        'generator_state_dict' :        generator.state_dict(),
+                        'discriminator_state_dict' :    discriminator.state_dict(),
+                        'optimizer_g_state_dict' :      optimizer_g.state_dict(),
+                        'optimizer_d_state_dict' :      optimizer_d.state_dict(),
+                        'g_adv':                        adversarial_loss.state_dict(),
+                        'g_pixel':                      pixelwise_loss.state_dict(),
+                      }
+        torch.save(check_point, check_points_path + 'check_point.pt')
+    print('Epoch %d/%d -- time  %d'%(epoch, epochs, time.time()-epcoch_start))
+
+log_loss_file.close()
+
+torch.save(generator.state_dict(), model_path + 'generator.pt')
+torch.save(discriminator.state_dict(), model_path + 'discriminator.pt')
+
+run_test(test_path, model_path, test_output_path)
---
+++ generator.py
@@ -0,0 +1,117 @@
+from torch import nn
+
+class generator(nn.Module):
+    '''
+    Generator
+    encoder-decoder setup
+    '''
+    def __init__(self, img_x=128, img_y=128, channels=3):
+        super().__init__() #for creating a instance of nn.module for inheritence
+        self.img_x, self.img_y = img_x, img_y
+        self.channels = channels
+
+        def down_sampling(input_channels=None, output_channels=None, normalize=True):
+            layers = list()
+
+            #Conv2d Inputs
+            args = {
+                        'in_channels':input_channels,
+                        'out_channels':output_channels,
+                        'stride':2,
+                        'padding':1,
+                        'kernel_size':(4,4)
+                   }
+
+            layers.append(nn.Conv2d(**args))
+
+            #normalize flag
+            if normalize:
+                layers.append(nn.BatchNorm2d(output_channels, .8))
+            
+            #activation
+            layers.append(nn.LeakyReLU(.2))
+
+            return layers
+        
+        def up_sampling(input_channels=None, output_channels=None, normalize=True):
+            layers = list()
+
+            #Conv2dTranspose Inputs
+            args = {
+                        'in_channels':input_channels,
+                        'out_channels': output_channels,
+                        'kernel_size':(4,4),
+                        'stride':2,
+                        'padding':1
+                   }
+            
+            layers.append(nn.ConvTranspose2d(**args))
+
+            #normalize flag
+            if normalize:
+                layers.append(nn.BatchNorm2d(output_channels, .8))
+            
+            #activation
+            layers.append(nn.ReLU())
+
+            return layers
+
+        def get_layers(layer_config_list, transpose=False):
+            layers = list()
+            
+            for layer_config in layer_config_list:
+                if not transpose:
+                    layers += down_sampling(**layer_config)
+                else:
+                    layers += up_sampling(**layer_config)
+
+            return layers
+        
+
+        def config_layer(input_channels=3,output_channels=3, normalize=True):
+            return locals()
+
+
+        self.encoder_layers = list()
+
+        #encoder_layers
+        self.encoder_layers.append(config_layer(self.channels, 64, False)) #-->layer 1
+
+        self.encoder_layers.append(config_layer(64, 64, True))              #-->layer 2
+                                   
+        self.encoder_layers.append(config_layer(64, 128, True))             #-->layer 3
+        
+        self.encoder_layers.append(config_layer(128, 256, True))            #-->layer 4
+
+        self.encoder_layers.append(config_layer(256, 512, True))            #-->layer 5
+
+        
+
+        self.decoder_layers = list()
+
+        #decoder_layers
+        self.decoder_layers.append(config_layer(4000, 512, True))           #-->layer 1
+
+        self.decoder_layers.append(config_layer(512, 256, True))            #-->layer 2
+
+        self.decoder_layers.append(config_layer(256, 128, True))            #-->layer 3
+        
+        self.decoder_layers.append(config_layer(128, 64, True))             #-->layer 4
+
+        #model
+        self.model = nn.Sequential(
+                                        #encode Layers
+                                        *get_layers(self.encoder_layers, transpose=False),
+                                        
+                                        #bootleneck
+                                        nn.Conv2d(in_channels=512, out_channels=4000, kernel_size=(1,1)),
+                                        
+                                        #decode Layers
+                                        *get_layers(self.decoder_layers, transpose=True),
+
+                                        nn.Conv2d(in_channels=64, out_channels=self.channels, kernel_size=(3,3), stride=1, padding=1),
+
+                                        nn.Tanh()
+                                  )
+    def forward(self, x):
+        return self.model(x)
